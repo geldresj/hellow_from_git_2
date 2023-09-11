479ac1703d10cb9f7033ae696645130fc0135ebd
Julio Geldres <geldresj@gmail.com>
2023-09-11 12:54:29 -0400

    reactnative stylus

---
+++ filenumber2.js
@@ -0,0 +1,16 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class filenumber2 extends Component {
+  render() {
+    return (
+      <View>
+        <Text>filenumber2</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
+//This is rnsc
+//This is line 16 of filenumber2
